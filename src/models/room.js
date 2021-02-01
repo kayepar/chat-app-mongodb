3cc8c6d06994e8cbc13782bb6ede02d96a48ba92
Kaye Par <kaye.cenizal@gmail.com>
2021-02-01 16:16:27 +0800

    Added models

---
+++ src/models/room.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const roomSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        required: true,
+    },
+});
+
+// roomSchema.statics.findByName = async (roomName) => {
+//     const room = await Room.findOne({ name: roomName });
+
+//     // if (!room) return null;
+
+//     return room;
+// };
+
+// used to create virtual properties --> not saved in Db
+roomSchema.virtual('users', {
+    ref: 'User',
+    localField: '_id',
+    foreignField: 'chatroom',
+});
+
+const Room = mongoose.model('Room', roomSchema);
+
+module.exports = Room;
---
+++ src/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+// const Room = require('./Room');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid.');
+            }
+        },
+    },
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        lowecase: true,
+    },
+    chatroom: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Room',
+    },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
