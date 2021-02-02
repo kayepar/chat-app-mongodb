5c3d3c31e8300bfca285b3da7e580169b1a39009
Kaye Par <kaye.cenizal@gmail.com>
2021-02-03 01:39:55 +0800

    Set focus on email field

--- public/js/index/index.js
+++ public/js/index/index.js
@@ -1,6 +1,8 @@
 const { getAvailableRooms } = require('./index-utils');
 
 $(document).ready(function () {
-    document.querySelector('#username-text').focus();
+    // document.querySelector('#username-text').focus();
+    document.querySelector('#email-text').focus();
+
     getAvailableRooms();
 });
