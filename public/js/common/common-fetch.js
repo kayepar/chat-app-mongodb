4459fcf828e2be17ff3956f0aade9ed5d23a098d
Kaye Par <kaye.cenizal@gmail.com>
2021-02-06 02:41:27 +0800

    Temporarily disabled fetch

--- public/js/common/common-fetch.js
+++ public/js/common/common-fetch.js
@@ -1,31 +1,37 @@
 const isUsernameValid = async (username, room) => {
-    const response = await fetch(`/user/isUsernameValid?username=${username}&room=${room}`);
+    // const response = await fetch(`/user/isUsernameValid?username=${username}&room=${room}`);
 
-    if (!response.ok) throw new Error(`An error has occured: ${response.status}`);
+    // if (!response.ok) throw new Error(`An error has occured: ${response.status}`);
 
-    const { isValid } = await response.json();
+    // const { isValid } = await response.json();
 
-    return isValid;
+    // return isValid;
+
+    return true;
 };
 
 const getAllActiveRooms = async () => {
-    const response = await fetch('/room/getAllActiveRooms');
+    // const response = await fetch('/room/getAllActiveRooms');
 
-    if (!response.ok) throw new Error(`An error has occured: ${response.status}`);
+    // if (!response.ok) throw new Error(`An error has occured: ${response.status}`);
 
-    const { rooms } = await response.json();
+    // const { rooms } = await response.json();
 
-    return rooms;
+    // return rooms;
+
+    return [];
 };
 
 const getAllOtherActiveRooms = async (myRoom) => {
-    const response = await fetch(`/room/getAllOtherActiveRooms?room=${myRoom}`);
+    // const response = await fetch(`/room/getAllOtherActiveRooms?room=${myRoom}`);
 
-    if (!response.ok) throw new Error(`An error has occured: ${response.status}`);
+    // if (!response.ok) throw new Error(`An error has occured: ${response.status}`);
 
-    const { rooms } = await response.json();
+    // const { rooms } = await response.json();
 
-    return rooms;
+    // return rooms;
+
+    return [];
 };
 
 module.exports = {
