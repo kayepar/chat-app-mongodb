993dd97a6123747f225b3b542f8c23ff8c2c207b
Kaye Par <kaye.cenizal@gmail.com>
2021-02-09 15:17:28 +0800

    Added user cleanup upon server restart

--- src/db/utils.js
+++ src/db/utils.js
@@ -1,6 +1,5 @@
 const User = require('../models/user');
 const Room = require('../models/room');
-const Message = require('../models/message');
 
 const cleanupDb = async () => {
     console.log('cleanup called');
@@ -12,18 +11,23 @@ const cleanupDb = async () => {
     // if no message other than admin - delete room and messages
     // otherwise, leave room as is
     try {
-        await User.deleteMany({});
-
-        await Message.deleteMany({});
         const rooms = await Room.find({});
-
         // console.log(rooms[0]);
 
-        rooms.forEach(async (room) => {
-            // console.log(room);
-            const users = await room.execPopulate('users');
-            // console.log(users.users);
-        });
+        // rooms.forEach(async (room) => {
+        //     // console.log(room);
+        //     await Room.deleteIfInactive(room);
+        // });
+
+        await Promise.all(
+            rooms.map(async (room) => {
+                await Room.deleteIfInactive(room, 'cleanup');
+            })
+        );
+
+        // !won't work if users are more than 1
+
+        await User.deleteMany({});
 
         //     .exec()
         //     .then(async (room) => {
