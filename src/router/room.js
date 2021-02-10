0eefc334a6fd8040c77686f4159d4d8ab81d1102
Kaye Par <kaye.cenizal@gmail.com>
2021-02-11 03:06:49 +0800

    Added validateUser route

--- src/router/room.js
+++ src/router/room.js
@@ -3,34 +3,19 @@ const router = new express.Router();
 const Room = require('../models/room');
 
 router.get('/validateUser', async (req, res) => {
-    console.log(req.query);
-
     try {
-        const room = await Room.find({ name: req.query.room });
-
-        console.log(`room: ${room}`);
+        const { email, username, room } = req.query;
+        const chatRoom = await Room.findOne({ name: room });
+        const isValid = chatRoom ? await chatRoom.validateUser(email, username) : true;
 
         res.status(200).send({
-            isValid: true,
+            isValid,
         });
     } catch (error) {
         res.status(400).send({
             error: error.message,
         });
     }
-    // const user = new User(req.body);
-    // try {
-    //     await user.save();
-    //     sendWelcomeEmail(user.email, user.name);
-    //     const token = await user.getAuthToken();
-    //     // res.status(201).send({user, token});
-    //     res.cookie('auth_token', token);
-    //     res.cookie('id', user.id);
-    //     res.sendFile(path.resolve(__dirname, '..', 'views', 'private.html'));
-    // } catch (error) {
-    //     // validation errors
-    //     res.status(400).send({ error: error.message });
-    // }
 });
 
 module.exports = router;
