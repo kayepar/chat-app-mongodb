b992b93a5dc666fc574ff7a2fac205ea865994b8
Kaye Par <kaye.cenizal@gmail.com>
2021-02-19 16:49:48 +0800

    Added error handling on mongoose connect

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-});
+// mongoose.connection.on('error', console.log.bind(console, 'connection refused !!!!!'));
+mongoose
+    .connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    })
+    .catch((error) => console.log('DB error encountered', error));
