7f97646510fb0c83066186075018cacbc046e46d
Kaye Par <kaye.cenizal@gmail.com>
2021-02-24 17:04:53 +0800

    Added CustomError class

---
+++ src/error/CustomError.js
@@ -0,0 +1,11 @@
+class CustomError extends Error {
+    constructor(message, status) {
+        super(message);
+
+        this.name = this.constructor.name;
+
+        this.status = status || 500;
+    }
+}
+
+module.exports = CustomError;
