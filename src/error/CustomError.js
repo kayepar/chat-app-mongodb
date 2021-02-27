220f142cfd132a9f25cb775606bac91484d1b774
Kaye Par <kaye.cenizal@gmail.com>
2021-02-28 00:34:51 +0800

    General code cleanup

--- src/error/CustomError.js
+++ src/error/CustomError.js
@@ -1,15 +1,15 @@
-const emailer = require('../email/emailer');
+const emailer = require('../utilities/emailer');
 
 class CustomError extends Error {
     constructor(message, cause, status = 500, sendEmail = false) {
         super(message);
+
         this.name = this.constructor.name;
         this.status = status;
         this.cause = cause;
+
         Error.captureStackTrace(this, this.constructor);
 
-        // console.log(`customerror cause: ${this.cause}`);
-        // console.log(`customerror stack: ${this.stack}`);
         if (sendEmail) {
             emailer.sendEmail(this);
         }
