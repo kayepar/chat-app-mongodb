f7cec7f4634c78e02dff8fd4aa58f02727d791bb
Kaye Par <kaye.cenizal@gmail.com>
2021-03-01 21:19:20 +0800

    Refactored router, added controller

---
+++ src/controller/room-controller.js
@@ -0,0 +1,45 @@
+const Room = require('../models/room');
+const CustomError = require('../error/CustomError');
+
+const validateUser = async (req, res, next) => {
+    try {
+        const { email, username, room } = req.query;
+
+        if (!email || !username || !room) {
+            throw new CustomError('Invalid request', 'Missing input', 400);
+        }
+
+        const chatRoom = await Room.findOne({ name: room });
+        // if room is not yet existing, credentials are automatically valid
+        const result = chatRoom ? chatRoom.validateUser(email, username) : { valid: true, duplicateFields: [] };
+
+        res.status(200).send({
+            result,
+        });
+    } catch (error) {
+        if (!(error instanceof CustomError)) {
+            return next(new CustomError('Something went wrong', error.stack, 500, true));
+        }
+
+        return next(error);
+    }
+};
+
+const getActiveRooms = (req, res, next) => {
+    try {
+        Room.getActiveRooms((error, rooms) => {
+            if (error) throw new CustomError('Something went wrong', error.stack, 500, true);
+
+            res.status(200).send({
+                rooms,
+            });
+        });
+    } catch (error) {
+        return next(error);
+    }
+};
+
+module.exports = {
+    validateUser,
+    getActiveRooms,
+};
--- src/router/room.js
+++ src/router/room.js
@@ -1,43 +1,9 @@
 const express = require('express');
+
 const router = new express.Router();
-const Room = require('../models/room');
-const CustomError = require('../error/CustomError');
+const { validateUser, getActiveRooms } = require('../controller/room-controller');
 
-router.get('/validateUser', async (req, res, next) => {
-    try {
-        const { email, username, room } = req.query;
-
-        if (!email || !username || !room) {
-            throw new CustomError('Invalid request', 'Missing input', 400);
-        }
-
-        const chatRoom = await Room.findOne({ name: room });
-        const result = chatRoom ? chatRoom.validateUser(email, username) : { valid: true, duplicateFields: [] };
-
-        res.status(200).send({
-            result,
-        });
-    } catch (error) {
-        if (!(error instanceof CustomError)) {
-            return next(new CustomError('Something went wrong', error.stack, 500, true));
-        }
-
-        return next(error);
-    }
-});
-
-router.get('/getActiveRooms', (req, res, next) => {
-    try {
-        Room.getActiveRooms((error, rooms) => {
-            if (error) throw new CustomError('Something went wrong', error.stack, 500, true);
-
-            res.status(200).send({
-                rooms,
-            });
-        });
-    } catch (error) {
-        return next(error);
-    }
-});
+router.get('/validateUser', validateUser);
+router.get('/getActiveRooms', getActiveRooms);
 
 module.exports = router;
