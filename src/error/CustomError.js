7e6e28190842dd581fa2304660d3b3bb852b0f9a
Kaye Par <kaye.cenizal@gmail.com>
2021-03-06 00:13:57 +0800

    Added 'message' property

--- src/error/CustomError.js
+++ src/error/CustomError.js
@@ -2,10 +2,11 @@ const emailer = require('../utilities/emailer');
 
 class CustomError extends Error {
     constructor(message, cause, status = 500, sendEmail = false) {
-        super(message);
+        super();
 
         this.name = this.constructor.name;
         this.status = status;
+        this.message = message;
         this.cause = cause;
 
         Error.captureStackTrace(this, this.constructor);
