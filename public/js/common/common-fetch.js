be87841998366f8b978b7f29ce28a8e7880c3f7c
Kaye Par <kaye.cenizal@gmail.com>
2021-03-07 00:52:02 +0800

    Code cleanup

--- public/js/common/common-fetch.js
+++ public/js/common/common-fetch.js
@@ -3,6 +3,7 @@ const CustomError = require('../../../src/error/CustomError');
 const validateUser = async (email, username, room) => {
     const response = await fetch(`/validateUser?email=${email}&username=${username}&room=${room}`);
 
+    // todo: test this again!
     if (!response.ok) throw new CustomError(`An error has occured in validateUser route`, response.statusText, status);
 
     const { result } = await response.json();
