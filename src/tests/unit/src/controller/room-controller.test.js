8bf2dfa364217ef5a94660bd2bc32c7e6ba7d8aa
Kaye Par <kaye.cenizal@gmail.com>
2021-03-11 02:20:43 +0800

    Added tests

--- src/tests/unit/src/controller/room-controller.test.js
+++ src/tests/unit/src/controller/room-controller.test.js
@@ -1,10 +1,15 @@
 jest.mock('../../../../error/CustomError');
 
+// jest.mock('../../../../models/room', () => ({
+//     isUserAllowedToJoin: jest.fn(),
+// }));
 // jest.mock('../../../../error/CustomError', function () {
 //     return this;
 // });
 
-const { validateUser, getActiveRooms } = require('../../../../controller/room-controller');
+// const RoomModel = require('../../../../models/room');
+// const isUserAllowedToJoin = require('../../../../models/room').isUserAllowedToJoin;
+const room_controller = require('../../../../controller/room-controller');
 const CustomError = require('../../../../error/CustomError');
 
 const mockRequest = (queryData) => ({
@@ -52,7 +57,7 @@ describe('tests for rooms-controller - mocked request and response', () => {
                 const res = mockResponse();
                 const next = mockNext;
 
-                await validateUser(req, res, next);
+                await room_controller.validateUser(req, res, next);
 
                 expect(next).toHaveBeenCalledWith(expect.objectContaining({ status: 400 }));
             });
@@ -62,7 +67,7 @@ describe('tests for rooms-controller - mocked request and response', () => {
                 const res = mockResponse();
                 const next = mockNext;
 
-                await validateUser(req, res, next);
+                await room_controller.validateUser(req, res, next);
 
                 expect(next).toHaveBeenCalledWith(expect.objectContaining({ status: 400 }));
             });
@@ -72,30 +77,117 @@ describe('tests for rooms-controller - mocked request and response', () => {
                 const res = mockResponse();
                 const next = mockNext;
 
-                await validateUser(req, res, next);
+                await room_controller.validateUser(req, res, next);
 
                 expect(next).toHaveBeenCalledWith(expect.objectContaining({ status: 400 }));
             });
         });
 
+        let getChatRoomMock;
+        let checkUserAccessMock;
+
         describe('valid responses', () => {
-            test('if room is not existing, should return HTTP 200 and isAllowed to true', () => {
-                // todo: try mockingoose - mock find() itseld and not getChatRoom function
-                const getChatRoom = jest.fn().mockReturnValue(null);
+            beforeEach(() => {
+                getChatRoomMock = jest.spyOn(room_controller, 'getChatRoom');
+                checkUserAccessMock = jest.spyOn(room_controller, 'checkUserAccess');
+            });
+
+            test('if room is not existing, should return HTTP 200, isAllowed should be true and duplicateFields empty', async () => {
+                getChatRoomMock.mockReturnValue(null);
+
+                const testResult = {
+                    result: { isAllowed: true, duplicateFields: [] },
+                };
 
                 const req = mockRequest({ email: 'kaye.cenizal@gmail.com', username: 'kaye', room: 'javascript' });
                 const res = mockResponse();
                 const next = mockNext;
 
-                const testResult = {
-                    isAllowed: true,
-                    duplicateFields: [],
-                };
-
-                validateUser(req, res, next);
+                await room_controller.validateUser(req, res, next);
 
                 expect(res.status).toHaveBeenCalledWith(200);
                 expect(res.send).toHaveBeenCalledWith(testResult);
+                expect(getChatRoomMock).toHaveBeenCalled();
+            });
+
+            test('if room existing but user is valid, should return HTTP 200, isAllowed should be true and duplicateFields empty', async () => {
+                getChatRoomMock.mockReturnValue({ _id: '6043163766eb61058c06d3f2', name: 'javascript', __v: 0 });
+                checkUserAccessMock.mockReturnValue({ isAllowed: true, duplicateFields: [] });
+
+                const testResult = {
+                    result: { isAllowed: true, duplicateFields: [] },
+                };
+
+                const req = mockRequest({ email: 'kaye.cenizal@gmail.com', username: 'kaye', room: 'javascript' });
+                const res = mockResponse();
+                const next = mockNext;
+
+                await room_controller.validateUser(req, res, next);
+
+                expect(res.status).toHaveBeenCalledWith(200);
+                expect(res.send).toHaveBeenCalledWith(testResult);
+                expect(getChatRoomMock).toHaveBeenCalled();
+                expect(checkUserAccessMock).toHaveBeenCalled();
+            });
+
+            test(`if room existing but email already in use, should return HTTP 200, isAllowed should be false and duplicateFields to be [ 'email' ]`, async () => {
+                getChatRoomMock.mockReturnValue({ _id: '6043163766eb61058c06d3f2', name: 'javascript', __v: 0 });
+                checkUserAccessMock.mockReturnValue({ isAllowed: false, duplicateFields: ['email'] });
+
+                const testResult = {
+                    result: { isAllowed: false, duplicateFields: ['email'] },
+                };
+
+                const req = mockRequest({ email: 'kaye.cenizal@gmail.com', username: 'kaye', room: 'javascript' });
+                const res = mockResponse();
+                const next = mockNext;
+
+                await room_controller.validateUser(req, res, next);
+
+                expect(res.status).toHaveBeenCalledWith(200);
+                expect(res.send).toHaveBeenCalledWith(testResult);
+                expect(getChatRoomMock).toHaveBeenCalled();
+                expect(checkUserAccessMock).toHaveBeenCalled();
+            });
+
+            test(`if room existing but username already in use, should return HTTP 200, isAllowed should be false and duplicateFields to be [ 'username' ]`, async () => {
+                getChatRoomMock.mockReturnValue({ _id: '6043163766eb61058c06d3f2', name: 'javascript', __v: 0 });
+                checkUserAccessMock.mockReturnValue({ isAllowed: false, duplicateFields: ['username'] });
+
+                const testResult = {
+                    result: { isAllowed: false, duplicateFields: ['username'] },
+                };
+
+                const req = mockRequest({ email: 'kaye.cenizal@gmail.com', username: 'kaye', room: 'javascript' });
+                const res = mockResponse();
+                const next = mockNext;
+
+                await room_controller.validateUser(req, res, next);
+
+                expect(res.status).toHaveBeenCalledWith(200);
+                expect(res.send).toHaveBeenCalledWith(testResult);
+                expect(getChatRoomMock).toHaveBeenCalled();
+                expect(checkUserAccessMock).toHaveBeenCalled();
+            });
+
+            test(`if room existing but both email and username already in use, should return HTTP 200, isAllowed should be false and duplicateFields to be [ 'email', 'username' ]`, async () => {
+                getChatRoomMock.mockReturnValue({ _id: '6043163766eb61058c06d3f2', name: 'javascript', __v: 0 });
+                checkUserAccessMock.mockReturnValue({ isAllowed: false, duplicateFields: ['email', 'username'] });
+
+                const testResult = {
+                    result: { isAllowed: false, duplicateFields: ['email', 'username'] },
+                };
+
+                const req = mockRequest({ email: 'kaye.cenizal@gmail.com', username: 'kaye', room: 'javascript' });
+                const res = mockResponse();
+                const next = mockNext;
+
+                await room_controller.validateUser(req, res, next);
+
+                expect(res.status).toHaveBeenCalledWith(200);
+                expect(res.send).toHaveBeenCalledWith(testResult);
+                expect(getChatRoomMock).toHaveBeenCalled();
+                expect(checkUserAccessMock).toHaveBeenCalled();
             });
         });
     });
