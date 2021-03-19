4beb320a447d804335142a785fc61426a861f547
Kaye Par <kaye.cenizal@gmail.com>
2021-03-19 12:39:44 +0800

    Added checking for input

--- src/utilities/input-utils.js
+++ src/utilities/input-utils.js
@@ -1,11 +1,19 @@
-// const sanitize = ({ email, username, room }) => {};
-
 const { check } = require('express-validator');
 
-// const validateEmail = () => {
-//     [check('email').isEmail()];
-// };
+const preSanitizeAndValidate = [
+    check('username').trim().not().isEmpty().withMessage('Missing input').bail(),
+    check('room').trim().not().isEmpty().withMessage('Missing input').bail(),
+    check('email')
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage('Missing input')
+        .bail()
+        .isEmail()
+        .withMessage('Invalid email')
+        .bail(),
+];
 
 module.exports = {
-    validateEmail: [check('email').isEmail()],
+    preSanitizeAndValidate,
 };
