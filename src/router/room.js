c59f9f5e1c66e9380713f82b1a333316a7ccddc5
Kaye Par <kaye.cenizal@gmail.com>
2021-03-19 20:59:44 +0800

    Refactored middleware name

--- src/router/room.js
+++ src/router/room.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const router = new express.Router();
 const { validateUser, getActiveRooms } = require('../controller/room-controller');
-const { preSanitizeAndValidate } = require('../utilities/input-utils');
+const { preValidateInput } = require('../utilities/input-utils');
 
-router.get('/validateUser', preSanitizeAndValidate, validateUser);
+router.get('/validateUser', preValidateInput, validateUser);
 router.get('/getActiveRooms', getActiveRooms);
 
 module.exports = router;
