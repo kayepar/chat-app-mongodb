31b316f8fb0d678d878a98dac2b825c406fede84
Kaye Par <kaye.cenizal@gmail.com>
2021-03-29 02:58:15 +0800

    Removed test inputs

--- public/js/index/index.js
+++ public/js/index/index.js
@@ -4,9 +4,5 @@ $(document).ready(function () {
     // document.querySelector('#username-text').focus();
     document.querySelector('#email-text').focus();
 
-    document.querySelector('#email-text').value = 'kaye.cenizal@gmail.com';
-    document.querySelector('#username-text').value = 'kaye';
-    document.querySelector('#room-text').value = 'one';
-
     displayAvailableRooms();
 });
