62c5e481a137d0858a1c3d19499e5ef366b24da5
Kaye Par <kaye.cenizal@gmail.com>
2021-03-31 02:39:15 +0800

    Set more options in server config

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,13 +1,17 @@
 module.exports = {
     launch: {
         headless: false,
-        slowMo: 40,
+        slowMo: 20,
         devtools: true,
         args: ['--window-size=1920,1080'],
         dumpio: true,
     },
     server: {
-        command: 'node src/server.js',
+        // command: 'node src/server.js',
+        command: 'npm run dev-test',
         port: 80,
+        launchTimeout: 50000,
+        usedPortAction: 'kill',
+        debug: true,
     },
 };
