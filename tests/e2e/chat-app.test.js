9532cabfddea97fdbc209e73fb204dbf9ab26036
Kaye Par <kaye.cenizal@gmail.com>
2021-04-01 01:00:26 +0800

    Added tests for second user

--- tests/e2e/chat-app.test.js
+++ tests/e2e/chat-app.test.js
@@ -3,8 +3,6 @@ require('../../jest-puppeteer.config');
 
 const timeout = 40000;
 
-let page2;
-
 beforeAll(async () => {
     await page.goto(URL, { waitUntil: 'domcontentloaded' });
 });
@@ -392,75 +390,307 @@ describe('end-to-end tests for chat app', () => {
     });
 
     describe.only('second user', () => {
+        let page2;
+
+        beforeAll(async () => {
+            page2 = await browser.newPage();
+            await page2.goto(URL, { waitUntil: 'domcontentloaded' });
+
+            await page.waitForTimeout(2000);
+        });
+
         describe('index page', () => {
-            test(
-                'should display available rooms button (with options hidden)',
-                async () => {
-                    page2 = await browser.newPage();
+            describe.skip('available rooms button', () => {
+                test(
+                    'should show button (with options hidden)',
+                    async () => {
+                        const is_available_rooms_shown = await page2.$eval(
+                            '#active-rooms',
+                            (element) => window.getComputedStyle(element).getPropertyValue('display') !== 'none'
+                        );
 
-                    await page2.goto(URL, { waitUntil: 'domcontentloaded' });
-                    await page.waitForTimeout(2000);
+                        expect(is_available_rooms_shown).toBe(true);
 
-                    const is_available_rooms_shown = await page2.$eval(
-                        '#active-rooms',
-                        (element) => window.getComputedStyle(element).getPropertyValue('display') !== 'none'
-                    );
+                        const active_rooms_menu = await page2.$('#active-rooms-menu');
+                        const active_rooms_menu_className = await page2.evaluate(
+                            (element) => element.className,
+                            active_rooms_menu
+                        );
 
-                    expect(is_available_rooms_shown).toBe(true);
+                        expect(active_rooms_menu_className).not.toContain('show');
+                    },
+                    timeout
+                );
 
-                    const active_rooms_menu = await page2.$('#active-rooms-menu');
-                    const active_rooms_menu_className = await page2.evaluate(
-                        (element) => element.className,
-                        active_rooms_menu
-                    );
+                test(
+                    'should show options when button is clicked',
+                    async () => {
+                        await page2.click('#active-rooms');
+                        await page.waitForTimeout(2000);
 
-                    expect(active_rooms_menu_className).not.toContain('show');
-                },
-                timeout
-            );
+                        const active_rooms_menu = await page2.$('#active-rooms-menu');
+                        const active_rooms_menu_className = await page2.evaluate(
+                            (element) => element.className,
+                            active_rooms_menu
+                        );
 
-            test(
-                'should show options when active rooms button is clicked',
-                async () => {
-                    await page2.click('#active-rooms');
-                    await page.waitForTimeout(2000);
+                        expect(active_rooms_menu_className).toContain('show');
+                    },
+                    timeout
+                );
 
-                    const active_rooms_menu = await page2.$('#active-rooms-menu');
-                    const active_rooms_menu_className = await page2.evaluate(
-                        (element) => element.className,
-                        active_rooms_menu
-                    );
+                test(
+                    'should have two options available',
+                    async () => {
+                        const options_count = await page2.$$eval('#active-rooms-menu a', (options) => options.length);
 
-                    expect(active_rooms_menu_className).toContain('show');
-                },
-                timeout
-            );
+                        expect(options_count).toBe(2);
+                    },
+                    timeout
+                );
 
-            test(
-                'should have two options available in active rooms dropdown',
-                async () => {
-                    const options_count = await page2.$$eval('#active-rooms-menu a', (options) => options.length);
+                test(
+                    'on hover, option should correctly change color',
+                    async () => {
+                        const first_option = await page2.$('#active-rooms-menu > a');
+                        await first_option.hover();
 
-                    expect(options_count).toBe(2);
-                },
-                timeout
-            );
+                        const bgcolor_on_hover = await page2.evaluate(
+                            (element) => window.getComputedStyle(element).getPropertyValue('background-color'),
+                            first_option
+                        );
 
-            test(
-                'on hover, active rooms options should correctly change color',
-                async () => {
-                    const first_option = await page2.$('#active-rooms-menu > a');
-                    await first_option.hover();
+                        expect(bgcolor_on_hover).not.toBe('rgb(255, 255, 255)'); // not white
+                    },
+                    timeout
+                );
 
-                    const bgcolor_on_hover = await page2.evaluate(
-                        (element) => window.getComputedStyle(element).getPropertyValue('background-color'),
-                        first_option
-                    );
+                test(
+                    'on click, option should be selected as active room',
+                    async () => {
+                        const option_text = await page2.$eval('#active-rooms-menu > a', (div) => div.innerHTML.trim());
 
-                    expect(bgcolor_on_hover).not.toBe('rgb(255, 255, 255)'); // not white
-                },
-                timeout
-            );
+                        await page2.click('#active-rooms-menu > a');
+
+                        const button_text = await page2.$eval('#active-room-text', (span) => span.innerHTML.trim());
+
+                        expect(button_text).toBe(option_text);
+                    },
+                    timeout
+                );
+
+                test(
+                    'on click, should clear room input value',
+                    async () => {
+                        await page2.click('#room-text');
+                        await page2.type('#room-text', 'test');
+
+                        await page2.click('#active-rooms');
+
+                        await page2.waitForSelector('#active-rooms-menu', { visible: true });
+
+                        await page2.click('#active-rooms-menu > a');
+
+                        const room_text_value = await page2.$eval('#room-text', (input) => input.value.trim());
+
+                        expect(room_text_value).toBe('');
+                    },
+                    timeout
+                );
+
+                test(
+                    `clear selection option, should reset button`,
+                    async () => {
+                        await page2.click('#active-rooms');
+                        await page2.waitForSelector('#active-rooms-menu', { visible: true });
+
+                        await page2.click('a#clear');
+
+                        const button_text = await page2.$eval('#active-room-text', (span) => span.innerHTML.trim());
+
+                        expect(button_text).toBe('Join an active room');
+                    },
+                    timeout
+                );
+            });
+
+            describe('modal window (room existing)', () => {
+                beforeAll(async () => {
+                    await page2.click('#email-text');
+                    await page2.type('#email-text', 'callie.par@gmail.com');
+
+                    await page2.click('#username-text');
+                    await page2.type('#username-text', 'callie');
+
+                    await page2.click('#room-text');
+                    await page2.type('#room-text', 'html');
+                });
+
+                test(
+                    'should be displayed if room already exists',
+                    async () => {
+                        await page2.keyboard.press('Enter');
+
+                        await page2.waitForSelector('#duplicate-room-modal', { visible: true });
+
+                        const duplicate_room_modal = await page2.$('#duplicate-room-modal');
+                        const duplicate_room_modal_className = await page2.evaluate(
+                            (element) => element.className,
+                            duplicate_room_modal
+                        );
+
+                        expect(duplicate_room_modal_className).toContain('show');
+                    },
+                    timeout
+                );
+
+                test(
+                    `if open and 'No' is clicked, should close modal`,
+                    async () => {
+                        await page2.waitForSelector('#duplicate-room-modal-no-button', { visible: true });
+
+                        await page2.click('#duplicate-room-modal-no-button');
+
+                        const duplicate_room_modal = await page2.$('#duplicate-room-modal-no-button');
+                        const duplicate_room_modal_className = await page2.evaluate(
+                            (element) => element.className,
+                            duplicate_room_modal
+                        );
+
+                        expect(duplicate_room_modal_className).not.toContain('show');
+                    },
+                    timeout
+                );
+
+                test(
+                    `if open and 'X' is clicked, should close modal`,
+                    async () => {
+                        await page2.waitForSelector('#duplicate-room-modal', { hidden: true });
+
+                        await page2.keyboard.press('Enter');
+
+                        await page2.waitForSelector('#duplicate-room-modal', { visible: true });
+
+                        await page2.waitForSelector('#duplicate-room-modal-x-button', { visible: true });
+                        await page2.click('#duplicate-room-modal-x-button');
+
+                        const duplicate_room_modal = await page2.$('#duplicate-room-modal-x-button');
+                        const duplicate_room_modal_className = await page2.evaluate(
+                            (element) => element.className,
+                            duplicate_room_modal
+                        );
+
+                        expect(duplicate_room_modal_className).not.toContain('show');
+                    },
+                    timeout
+                );
+
+                test.only(
+                    `if open and 'Yes' is clicked, should auto-select room in active rooms dropdown`,
+                    async () => {
+                        await page2.waitForSelector('#duplicate-room-modal', { hidden: true });
+
+                        await page2.keyboard.press('Enter');
+
+                        await page2.waitForSelector('#duplicate-room-modal', { visible: true });
+
+                        await page2.waitForSelector('#duplicate-room-modal-yes-button', { visible: true });
+                        await page2.click('#duplicate-room-modal-yes-button');
+
+                        await page2.waitForSelector('#duplicate-room-modal', { hidden: true });
+                        const button_text = await page2.$eval('#active-room-text', (span) => span.innerHTML.trim());
+
+                        expect(button_text).not.toBe('Join an active room');
+                    },
+                    timeout
+                );
+            });
+
+            describe.only('input validation (duplicate credentials)', () => {
+                // todo: both email and username
+                test(
+                    'if duplicate email, should display error message',
+                    async () => {
+                        await page2.keyboard.press('Enter');
+
+                        const is_duplicate_email_error_shown = await page2.$eval(
+                            '#email-feedback',
+                            (element) => window.getComputedStyle(element).getPropertyValue('display') !== 'none'
+                        );
+
+                        expect(is_duplicate_email_error_shown).toBe(true);
+                    },
+                    timeout
+                );
+
+                test(
+                    'if email is changed, should hide error message',
+                    async () => {
+                        const emailInput = await page2.$('#email-text');
+                        await emailInput.click({ clickCount: 3 });
+                        await page2.keyboard.press('Backspace');
+
+                        await page2.click('#email-text');
+                        await page2.type('#email-text', 'kaye.cenizal@gmail.com');
+
+                        const is_duplicate_email_error_shown = await page2.$eval(
+                            '#email-feedback',
+                            (element) => window.getComputedStyle(element).getPropertyValue('display') !== 'none'
+                        );
+
+                        expect(is_duplicate_email_error_shown).toBe(false);
+
+                        // await jestPuppeteer.debug();
+                    },
+                    timeout
+                );
+
+                test(
+                    'if duplicate username, should display error message',
+                    async () => {
+                        // const usernameInput = await page2.$('#username-text');
+                        // await usernameInput.click({ clickCount: 3 });
+                        // await page2.keyboard.press('Backspace');
+
+                        // await page2.click('#username-text');
+                        // await page2.type('#username-text', 'callie');
+
+                        await page2.keyboard.press('Enter');
+
+                        const is_duplicate_username_error_shown = await page2.$eval(
+                            '#username-feedback',
+                            (element) => window.getComputedStyle(element).getPropertyValue('display') !== 'none'
+                        );
+
+                        expect(is_duplicate_username_error_shown).toBe(true);
+
+                        // await jestPuppeteer.debug();
+                    },
+                    timeout
+                );
+
+                test.skip(
+                    'if username is changed, should hide error message',
+                    async () => {
+                        const usernameInput = await page2.$('#username-text');
+                        await usernameInput.click({ clickCount: 3 });
+                        await page2.keyboard.press('Backspace');
+
+                        await page2.click('#username-text');
+                        await page2.type('#username-text', 'kaye');
+
+                        const is_duplicate_username_error_shown = await page2.$eval(
+                            '#username-feedback',
+                            (element) => window.getComputedStyle(element).getPropertyValue('display') !== 'none'
+                        );
+
+                        expect(is_duplicate_username_error_shown).toBe(false);
+
+                        // await jestPuppeteer.debug();
+                    },
+                    timeout
+                );
+            });
         });
     });
 });
