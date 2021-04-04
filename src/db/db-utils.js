98ffa75285d48629b96afc6facffa11eeee4e12c
Kaye Par <kaye.cenizal@gmail.com>
2021-04-04 16:07:54 +0800

    Added resetDb() - invoked when running tests

--- src/db/db-utils.js
+++ src/db/db-utils.js
@@ -1,11 +1,15 @@
 const User = require('../models/user');
 const Room = require('../models/room');
+const Message = require('../models/message');
+
 const logger = require('../utilities/logger');
 
 // Utility that will delete all users and inactive rooms
 // Called when server is started
 const cleanupDb = async () => {
     try {
+        if (process.env.NODE_ENV === 'test') return await resetDb();
+
         const rooms = await getRooms();
 
         await Promise.all(
@@ -22,6 +26,12 @@ const cleanupDb = async () => {
 
 const getRooms = async () => {
     return await Room.find({});
+};
+
+const resetDb = async () => {
+    await Room.deleteMany();
+    await User.deleteMany();
+    await Message.deleteMany();
 };
 
 module.exports = {
