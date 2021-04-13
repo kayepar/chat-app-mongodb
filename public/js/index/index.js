00bb49a627799396fd5ea1808d6ed1a7e058debd
Kaye Par <kaye.cenizal@gmail.com>
2021-04-14 02:23:36 +0800

    Removed unused code

--- public/js/index/index.js
+++ public/js/index/index.js
@@ -1,12 +1,7 @@
 const { displayAvailableRooms } = require('./index-utils');
 
 $(document).ready(function () {
-    // document.querySelector('#username-text').focus();
     document.querySelector('#email-text').focus();
-
-    // document.querySelector('#email-text').value = 'kaye.cenizal@gmail.com';
-    // document.querySelector('#username-text').value = 'kaye';
-    // document.querySelector('#room-text').value = 'one';
 
     displayAvailableRooms();
 });
